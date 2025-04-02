65d39833d5a71ad6dad604e5243564aa1f7e2b84
Alvin <113426752+AlvinTYJ@users.noreply.github.com>
2025-04-03 02:41:31 +0800

    Add files via upload

---
+++ e94115011_practice_4_1/e94115011_practice_4_1/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace e94115011_practice_4_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.tabControl1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Location = new System.Drawing.Point(27, 29);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(860, 34);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 29);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(852, 1);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "斗哥";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 29);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(852, 1);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "楷特";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(31, 522);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(736, 26);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(773, 522);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(110, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.White;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Location = new System.Drawing.Point(31, 69);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(852, 442);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.BackColor = System.Drawing.Color.White;
+            this.richTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox2.Location = new System.Drawing.Point(31, 69);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(852, 442);
+            this.richTextBox2.TabIndex = 4;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 587);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ e94115011_practice_4_1/e94115011_practice_4_1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
+
+namespace e94115011_practice_4_1
+{
+    public partial class Form1 : Form
+    {
+        private Color Chose;
+        private Color[] tabColors = new Color[2];
+
+        public Form1()
+        {
+            InitializeComponent();
+            tabControl1.SelectedIndexChanged += TabControl1_SelectedIndexChanged;
+
+            richTextBox1.ReadOnly = true;
+            richTextBox2.ReadOnly = true;
+            richTextBox1.BackColor = this.BackColor;
+            richTextBox2.BackColor = this.BackColor;
+
+            if (tabControl1.SelectedIndex == 0)
+            {
+                Tab1DisplayMode();
+            }
+
+            richTextBox1.DoubleClick += Form1_DoubleClick;
+            richTextBox2.DoubleClick += Form1_DoubleClick;
+            this.DoubleClick += Form1_DoubleClick;
+        }
+
+        private void TabControl1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (tabControl1.SelectedIndex == 0)
+            {
+                Tab1DisplayMode();
+            }
+            else
+            {
+                Tab2DisplayMode();
+            }
+        }
+
+        private void Tab1DisplayMode()
+        {
+            textBox1.Enabled = false;
+            button1.Enabled = false;
+            richTextBox1.Visible = true;
+            richTextBox2.Visible = false;
+            this.BackColor = tabColors[0];
+        }
+
+        private void Tab2DisplayMode()
+        {
+            textBox1.Enabled = true;
+            button1.Enabled = true;
+            richTextBox1.Visible = false;
+            richTextBox2.Visible = true;
+            this.BackColor = tabColors[1];
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                string message = textBox1.Text;
+                textBox1.Clear();
+
+                if (tabControl1.SelectedIndex == 0)
+                {
+                    return;
+                }
+                else
+                {
+                    richTextBox1.SelectionAlignment = HorizontalAlignment.Left;
+                    richTextBox1.AppendText($"{tabPage2.Text}: {message}" + Environment.NewLine);
+                    richTextBox1.SelectionAlignment = HorizontalAlignment.Right;
+                    richTextBox1.AppendText($"{tabPage1.Text}: 汪！" + Environment.NewLine);
+                    richTextBox1.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox1.ScrollToCaret();
+
+                    richTextBox2.SelectionAlignment = HorizontalAlignment.Right;
+                    richTextBox2.AppendText($"{tabPage2.Text}: {message}" + Environment.NewLine);
+                    richTextBox2.SelectionAlignment = HorizontalAlignment.Left;
+                    richTextBox2.AppendText($"{tabPage1.Text}: 汪！" + Environment.NewLine);
+                    richTextBox2.SelectionStart = richTextBox1.Text.Length;
+                    richTextBox2.ScrollToCaret();
+                }
+            }
+        }
+
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            顔色選擇 form2 = new 顔色選擇();
+            if (form2.ShowDialog() == DialogResult.OK)
+            {
+                Chose = form2.SelectedColor;
+                tabColors[tabControl1.SelectedIndex] = Chose;
+                if (tabControl1.SelectedIndex == 0)
+                {
+                    richTextBox1.BackColor = tabColors[0];
+                    this.BackColor = tabColors[0];
+                }
+                else
+                {
+                    richTextBox2.BackColor = tabColors[1];
+                    this.BackColor = tabColors[1];
+                }
+            }
+        }
+    }
+}
